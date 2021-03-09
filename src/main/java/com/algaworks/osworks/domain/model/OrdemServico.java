854db1791f3936cb2dd1d185fd77173ebbaf1c2c
José Paulo <jose_paulo_sjc@hotmail.com>
2021-03-09 14:57:33 -0300

    Curso AlgaWorks Spring Rest

---
+++ src/main/java/com/algaworks/osworks/domain/model/OrdemServico.java
@@ -0,0 +1,160 @@
+package com.algaworks.osworks.domain.model;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.groups.ConvertGroup;
+import javax.validation.groups.Default;
+
+import com.algaworks.osworks.domain.ValidationGroups;
+import com.algaworks.osworks.domain.exception.NegocioException;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+@Entity
+public class OrdemServico {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+	
+	@Valid
+	@ConvertGroup(from = Default.class, to = ValidationGroups.ClienteId.class)
+	@NotNull
+	@ManyToOne
+	@JoinColumn(name = "cliente_id")
+	private Cliente cliente;
+	
+	@NotBlank
+	@Column(name = "descricao")
+	private String descricao;
+	
+	@NotNull
+	@Column(name = "preco")
+	private BigDecimal preco;
+	
+	@JsonProperty(access = Access.READ_ONLY)
+	@Enumerated(EnumType.STRING	)
+	@Column(name = "status")
+	private StatusOrdemServico status;
+	
+	@JsonProperty(access = Access.READ_ONLY)
+	@Column(name = "data_abertura")
+	private OffsetDateTime dataAbertura;
+	
+	@JsonProperty(access = Access.READ_ONLY)
+	@Column(name = "data_finalizacao")
+	private OffsetDateTime dataFinalizacao;
+	
+	@OneToMany(mappedBy = "ordemServico")
+	private List<Comentario> comentarios = new ArrayList<>();
+	
+	public Long getId() {
+		return Id;
+	}
+	public void setId(Long id) {
+		Id = id;
+	}
+	public Cliente getCliente() {
+		return cliente;
+	}
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public BigDecimal getPreco() {
+		return preco;
+	}
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public StatusOrdemServico getStatus() {
+		return status;
+	}
+	public void setStatus(StatusOrdemServico status) {
+		this.status = status;
+	}
+	public OffsetDateTime getDataAbertura() {
+		return dataAbertura;
+	}
+	public void setDataAbertura(OffsetDateTime dataAbertura) {
+		this.dataAbertura = dataAbertura;
+	}
+	public OffsetDateTime getDataFinalizacao() {
+		return dataFinalizacao;
+	}
+	public void setDataFinalizacao(OffsetDateTime dataFinalizacao) {
+		this.dataFinalizacao = dataFinalizacao;
+	}
+	
+	public List<Comentario> getComentarios() {
+		return comentarios;
+	}
+	public void setComentarios(List<Comentario> comentarios) {
+		this.comentarios = comentarios;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrdemServico other = (OrdemServico) obj;
+		if (Id == null) {
+			if (other.Id != null)
+				return false;
+		} else if (!Id.equals(other.Id))
+			return false;
+		return true;
+	}
+	
+	
+	public boolean podeSerFinalizada(){
+		return StatusOrdemServico.ABERTA.equals(getStatus());
+	}
+	
+	public boolean naoPodeSerFinalizada(){
+		return !podeSerFinalizada();
+	}
+	
+	public void finalizar() {
+		
+		if(naoPodeSerFinalizada()) {
+			throw new NegocioException("Ordem de servico não pode ser finalizada");
+		}
+		setStatus(StatusOrdemServico.FINALIZADA);
+		setDataFinalizacao(OffsetDateTime.now());
+	}
+
+}
